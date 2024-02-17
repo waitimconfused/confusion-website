897de596f3df8df852ea027e1892d4e257c080c9
Dev-384 <106610123+Dev-384@users.noreply.github.com>
2024-02-17 18:58:12 -0500

    Almost finalized home page?

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,5 +1,59 @@
-import { site } from "../builder.js";
+import packageJSON from "../../package.json" assert {type: "json"};
 
-alert("Hello World!")
+document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
 
-site.header.setTitle("Hello World!");
+let asideData = packageJSON.index.asideContent;
+
+const asideElement = document.getElementsByTagName("aside")[0];
+asideElement.innerHTML = "";
+const mainElement = document.getElementsByTagName("main")[0];
+
+Object.keys(asideData).forEach(header => {
+	console.log(header);
+	let children = asideData[header];
+
+	let sectionDiv = document.createElement("section");
+	asideElement.appendChild(sectionDiv);
+
+	let sectionHeader = document.createElement("h1");
+	sectionHeader.innerText = header;
+	sectionDiv.appendChild(sectionHeader);
+
+	let childrenDiv = document.createElement("div");
+	sectionDiv.appendChild(childrenDiv);
+
+
+	children.forEach((element) => {
+		if(element.type == "text") text(element, childrenDiv);
+		if(element.type == "link") link(element, childrenDiv);
+		if(element.type == "image") image(element, childrenDiv);
+	});
+});
+
+function text(element, parentDiv){
+	let childDiv = document.createElement("div");
+	parentDiv.appendChild(childDiv);
+
+	let child = document.createElement("p");
+	child.innerText = element.text;
+	childDiv.appendChild(child);
+}
+
+function link(element, parentDiv){
+	// let childDiv = document.createElement("div");
+	// parentDiv.appendChild(childDiv);
+
+	let child = document.createElement("a");
+	child.innerText = element.text;
+	child.href = element.href;
+	parentDiv.appendChild(child);
+}
+
+function image(element, parentDiv){
+	let childDiv = document.createElement("div");
+	parentDiv.appendChild(childDiv);
+
+	let child = document.createElement("img");
+	child.src = element.src;
+	childDiv.appendChild(child);
+}
