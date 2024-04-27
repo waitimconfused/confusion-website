a69cfb011c807fce9ee12808ada1a6c2da685904
Dev-384 <elliott_3@loonielane.ca>
2024-04-27 14:13:40 -0400

    Updates to project/js_node
    Also changed "nodemon" to just "node" in `package.json`

--- projects/demo/js_node/index.js
+++ projects/demo/js_node/index.js
@@ -52,6 +52,8 @@ function graphUpdate(){
 	delta = (Date.now() - lastCalledTime)/1000;
 	lastCalledTime = Date.now();
 	fps = 1/delta;
+	
+	camera.zoom = Math.max(camera.zoom, 0.1);
 
 	cameraGlide();
 	if(customScript){
@@ -115,7 +117,7 @@ window.addEventListener('wheel', (e) => {
 	if (e.ctrlKey) {
 		e.preventDefault();
 		if(e.target !== globalGraph.canvas) return undefined;
-		camera.zoom = Math.max(camera.zoom - e.deltaY * 0.01, 0.1);
+		camera.zoom -= e.deltaY * 0.01;
 	} else {
 		if(e.target !== globalGraph.canvas) return undefined;
 		e.preventDefault();
@@ -128,30 +130,75 @@ window.addEventListener("gesturestart", (e) => e.preventDefault(), {passive: fal
 window.addEventListener("gesturechange", (e) => e.preventDefault(), {passive: false});
 window.addEventListener("gestureend", (e) => e.preventDefault(), {passive: false});
 
-let lastTouchX = null;
-let lastTouchY = null;
-document.ontouchstart = (e) => {
-	var touch = e.touches[0];
-	lastTouchX = touch.pageX;
-	lastTouchY = touch.pageY;
-}
+var lastTouchX = null;
+var lastTouchY = null;
+var lastZoomDistance = null;
+var hoveredNode = null;
+var numberOfTouches = 0;
+document.addEventListener("touchstart", function(e){
+	numberOfTouches == e.touches.length;
+	if(e.touches.length == 1){
+		var touch = e.touches[0];
+		lastTouchX = touch.pageX;
+		lastTouchY = touch.pageY;
+		mouse.position.x = lastTouchX;
+		mouse.position.y = lastTouchY;
+		mouse.click_l = true;
+	}else if(e.touches.length == 2){
+		lastZoomDistance = calcDistance({
+			x: e.touches[0].pageX,
+			y: e.touches[0].pageY
+		}, {
+			x: e.touches[1].pageX,
+			y: e.touches[2].pageY
+		});
+	}
+});
 document.addEventListener('touchmove', function(e) {
     e.preventDefault();
-    var touch = e.touches[0];
-	if(globalGraph.hasHoveredNode == false){
-		cameraMoveby(
-			-(lastTouchX - touch.pageX),
-			-(lastTouchY - touch.pageY)
-		);
-	}else{
-		globalGraph.hoveredNode.moveTo(
-			touch.pageX - globalGraph.canvas.width / 2 + camera.x * camera.zoom,
-			touch.pageY - globalGraph.canvas.height / 2 + camera.y * camera.zoom
-		);
+	if(globalGraph.hasHoveredNode){
+		hoveredNode = globalGraph.hoveredNode;
 	}
-	lastTouchX = touch.pageX;
-	lastTouchY = touch.pageY;
+	if(e.touches.length > numberOfTouches) numberOfTouches = e.touches.length;
+    if(e.touches.length == 1 && numberOfTouches == 1){
+		var touch = e.touches[0];
+		mouse.position.x = touch.pageX;
+		mouse.position.y = touch.pageY;
+		mouse.click_l = true;
+		if(!hoveredNode){
+			cameraMoveby(
+				-(lastTouchX - touch.pageX),
+				-(lastTouchY - touch.pageY)
+			);
+		}else{
+			hoveredNode.moveTo(
+				touch.pageX - globalGraph.canvas.width / 2 + camera.x * camera.zoom,
+				touch.pageY - globalGraph.canvas.height / 2 + camera.y * camera.zoom
+			);
+		}
+		lastTouchX = touch.pageX;
+		lastTouchY = touch.pageY;
+	}else if(e.touches.length == 2  && numberOfTouches == 2){
+		let currentZoomDistance = calcDistance({
+			x: e.touches[0].pageX,
+			y: e.touches[0].pageY
+		}, {
+			x: e.touches[1].pageX,
+			y: e.touches[1].pageY
+		});
+		if(!lastZoomDistance) lastZoomDistance = currentZoomDistance;
+		camera.zoom -= (lastZoomDistance - currentZoomDistance) * 0.01;
+		lastZoomDistance = currentZoomDistance;
+	}
 }, false);
+document.addEventListener("touchend", function(e){
+	if(e.touches.length == 0){
+		hoveredNode = null;
+		mouse.click_l = false;
+		lastZoomDistance = null;
+		numberOfTouches = 0;
+	}
+});
 
 export function applyFocus(node){
 	focusedNode = node;
@@ -216,9 +263,6 @@ export function lerp(a, b, t) {
 	t = Math.max(Math.min(t, 1), 0);
 	return a + (b - a) * t
 }
-
-
-
 
 export function hexToRgb(hex="") {
 	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
