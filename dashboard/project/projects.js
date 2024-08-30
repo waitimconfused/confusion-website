3f4d0d1859635f49ac72ae4ef5939c754bac5853
Blockbuilder384 <elliott_3@loonielane.ca>
2024-08-30 18:00:35 -0400

    STYLES!

--- dashboard/project/projects.js
+++ dashboard/project/projects.js
@@ -1,9 +1,8 @@
-import packageJSON from "/package.json" with {type: "json"};
 import { reloadTemplateElements } from "https://waitimconfused.github.io/confusion-projects/components/index-module.js";
 
 await reloadTemplateElements();
 
-var animateThemeChange = true;
+var animateThemeChange = false;
 
 const nav = document.querySelector("nav");
 const navMenu = document.querySelector("nav .menu");
@@ -11,55 +10,6 @@ const navMenuToggle = document.querySelector("nav .menu .icon-burger_menu");
 const navLinks = document.querySelector("nav .links");
 const themeToggleButton = document.getElementById("theme-toggle");
 const metaThemeColour = document.createElement("meta");
-
-const banner = new class Banner {
-	content = "";
-	colour = "accent";
-
-	constructor() {
-		this.setColour("accent");
-	}
-
-	show(skipAnimation=false) {
-		if (this.content == "") return;
-		if (skipAnimation || window.scrollY > 10) {
-			document.querySelector("nav .banner").style.transitionDuration = "0ms";
-			document.querySelector("nav").style.transitionDuration = "0ms";
-			setTimeout(() => {
-				document.querySelector("nav .banner").style.transitionDuration = null;
-				document.querySelector("nav").style.transitionDuration = null;
-			}, 450);
-		}
-		document.querySelector("nav .banner").classList.add("show");
-	}
-	hide() {
-		document.querySelector("nav .banner").classList.remove("show");
-	}
-	toggle() {
-		if (this.content == "") return;
-		document.querySelector("nav .banner").classList.toggle("show");
-	}
-	setContent(text="") {
-		this.content = text;
-		document.querySelector("nav .banner").innerHTML = `<p>${this.content}</p>`;
-		this.show();
-	}
-	setColour(colour="") {
-		document.querySelector("nav .banner").style.backgroundColor = `var(--${colour})`;
-		this.colour = colour;
-	}
-}
-
-if (packageJSON?.banner) {
-	let bannerText = packageJSON.banner;
-	bannerText = bannerText.replaceAll("\t", " <span style='margin-left: 1rem'></span> ");
-	bannerText = bannerText.replaceAll(/\*\*(.+?)\*\*/gm, "<strong>$1</strong>");
-	bannerText = bannerText.replaceAll("\n", " — ");
-	bannerText = bannerText.replaceAll(/\[(.+?)\]\((.+?)\)/gm, "<a href='$2'>$1</a>");
-	banner.setContent(bannerText);
-	banner.setColour("accent");
-	banner.show(true);
-}
 
 document.querySelector("nav .sitename").onmouseenter = (e) => {
 	document.querySelector("nav .sitename .img").classList.add("animate");
@@ -88,21 +38,8 @@ metaThemeColour.content = getComputedStyle(document.documentElement).getProperty
 document.head.appendChild(metaThemeColour);
 
 document.onscroll = (e) => {
-
 	navLinks.classList.remove("show");
-
-	if (banner.content) {
-		if ( window.scrollY > 10 ) {
-			document.querySelector("nav").classList.add("scroll");
-		} else {
-			document.querySelector("nav").classList.remove("scroll");
-		}
-	}
 }
-// window.onresize = () => {
-// 	document.querySelector("header .explode").style.setProperty("--window-width", window.innerWidth)
-// }
-// window.onresize();
 
 function toggleTheme() {
 	let theme = localStorage.getItem("theme");
