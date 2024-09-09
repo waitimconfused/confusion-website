193c03922645238bfb8d20274fc57dcaf4e3a5cb
Blockbuilder384 <elliott_3@loonielane.ca>
2024-09-09 19:29:57 -0400

    Cleanup + Styling
    Changing `dashboard.css` and `theme.css`
    
    Renaming files, and moving `Asterisk` to node module `asterisk-server`

--- host.js
+++ host.js
@@ -1,7 +1,6 @@
-import * as Ast from "./Asterisk/index.js";
+const Ast = require("asterisk-server");
 
 Ast.clearLogs();
 Ast.serverside.open(1200);
 Ast.files.regester404("./404.html");
 Ast.api.lock();
-// Ast.files.lock();
